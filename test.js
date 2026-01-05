b62504dca9f517e2bb9a20512779a777c1b51a47
yerbah-dan <dan@yerbah.com>
2026-01-05 10:34:34 -0600

    add test file with locked code

---
+++ test.js
@@ -0,0 +1,17 @@
+// Regular code here
+function helper() {
+  return true;
+}
+
+// @ai-lock
+function criticalAuth() {
+  // DO NOT MODIFY - security critical
+  const secret = "abc123";
+  return secret;
+}
+// @ai-unlock
+
+// More regular code
+function anotherHelper() {
+  return false;
+}
